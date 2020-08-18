ae01878eda218f2cd704e3edb211461273eebf26
Akanksha Chand <chandakanksha06@gmail.com>
2020-08-18 00:46:57 -0500

    commit

--- index.js
+++ index.js
@@ -3,48 +3,58 @@ const app = express();
 const exphbs  = require('express-handlebars');
 const path = require('path');
 const request = require('request');
-var hbs = require('handlebars')
+var hbs = require('handlebars');
+const fetch = require('node-fetch');
 
 const PORT = process.env.PORT || 3000;
 
 //API call to get pricing of Bitcoin from Bitstamp
 function call_api_btc_1(finishedAPI){
-	request('https://www.bitstamp.net/api/ticker/', {json : true}, (err, res, body) => {
-	if (err) {return console.log(err);}
-	if (res.statusCode === 200){
-		finishedAPI(body);
-		};
-	});	
+	fetch('https://www.bitstamp.net/api/ticker/')
+	.then(function (response) {
+		return response.json();
+	}).then(function (data) {
+ 		console.log(data);
+ 		finishedAPI(data);
+	}).catch(function (error) {
+		console.log(error);
+	});
 }
 
 //API call to get pricing of Ethereum from Bitstamp
 function call_api_eth_1(finishedAPI){
-	request('https://www.bitstamp.net/api/v2/ticker/ethusd/', {json : true}, (err, res, body) => {
-	if (err) {return console.log(err);}
-	if (res.statusCode === 200){
-		finishedAPI(body);
-		};
-	});	
+	fetch('https://www.bitstamp.net/api/v2/ticker/ethusd')
+	.then(function (response) {
+		return response.json();
+	}).then(function (data) {
+ 		finishedAPI(data);
+	}).catch(function (error) {
+		console.log(error);
+	});
 }
 
 //API call to get pricing of Bitcoin from Alpha Vantage
 function call_api_btc_2(finishedAPI){
-	request('https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=USD&apikey=HXBRO8DTOYT11XBZ', {json : true}, (err, res, body) => {
-	if (err) {return console.log(err);}
-	if (res.statusCode === 200){
-		finishedAPI(body);
-		};
+	fetch('https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=USD&apikey=JQ2BFSZC1HVHW6LW')
+	.then(function (response) {
+		return response.json();
+	}).then(function (data) {
+ 		finishedAPI(data);
+	}).catch(function (error) {
+		console.log(error);
 	});	
 }
 
 //API call to get pricing of Ethereum from Alpha Vantage
 function call_api_eth_2(finishedAPI){
-	request('https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=ETH&to_currency=USD&apikey=HXBRO8DTOYT11XBZ', {json : true}, (err, res, body) => {
-	if (err) {return console.log(err);}
-	if (res.statusCode === 200){
-		finishedAPI(body);
-		};
-	});	
+	fetch('https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=ETH&to_currency=USD&apikey=JQ2BFSZC1HVHW6LW')
+	.then(function (response) {
+		return response.json();
+	}).then(function (data) {
+ 		finishedAPI(data);
+	}).catch(function (error) {
+		console.log(error);
+	});
 }
 
 //Set Handlebars Middleware
@@ -52,41 +62,48 @@ app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
 
+var obj_btc_1;
+var obj_eth_1;
+var obj_btc_bid_2;
+var obj_btc_ask_2;
+var obj_eth_bid_2;
+var obj_eth_ask_2;
 app.get('/', function (req, res) {
-    call_api_btc_1(function(doneAPIbtc){    	
-    	call_api_eth_1(function(doneAPIeth){    		
-    		call_api_btc_2(function(doneAPIbtc2){
-    			var obj_btc_bid_2_st = doneAPIbtc2['Realtime Currency Exchange Rate']['8. Bid Price'].toString()
-    			var obj_btc_bid_2 = parseFloat(obj_btc_bid_2_st).toFixed(2)
-    			var obj_btc_ask_2_st = doneAPIbtc2['Realtime Currency Exchange Rate']['9. Ask Price'].toString()
-    			var obj_btc_ask_2 = parseFloat(obj_btc_ask_2_st).toFixed(2)
-    			call_api_eth_2(function(doneAPIeth2){
-    				var obj_eth_bid_2_st = doneAPIeth2['Realtime Currency Exchange Rate']['8. Bid Price'].toString()
-    				var obj_eth_bid_2 = parseFloat(obj_eth_bid_2_st).toFixed(2)
-    				var obj_eth_ask_2_st = doneAPIeth2['Realtime Currency Exchange Rate']['9. Ask Price'].toString()
-		    		var obj_eth_ask_2 = parseFloat(obj_eth_ask_2_st).toFixed(2)
-			    	res.render('home.handlebars', {
-			    		btc_bid_1: doneAPIbtc.bid,
-			    		btc_ask_1: doneAPIbtc.ask,
-			    		eth_bid_1: doneAPIeth.bid,
-			    		eth_ask_1: doneAPIeth.ask,
-			    		btc_bid_2: obj_btc_bid_2,
-			    		btc_ask_2: obj_btc_ask_2,
-			    		eth_bid_2: obj_eth_bid_2,
-			    		eth_ask_2: obj_eth_ask_2,
-			    		btc_bid_min : parseFloat(doneAPIbtc.bid)<=obj_btc_bid_2 ? "Bitstamp" : "Alpha Vantage",
-			    		btc_ask_max : parseFloat(doneAPIbtc.ask)>=obj_btc_ask_2 ? "Bitstamp" : "Alpha Vantage",
-			    		eth_bid_min : parseFloat(doneAPIeth.bid)<=obj_eth_bid_2 ? "Bitstamp" : "Alpha Vantage",
-			    		eth_ask_max : parseFloat(doneAPIeth.ask)>=obj_eth_ask_2 ? "Bitstamp" : "Alpha Vantage",
-			    		btc_url : "btc.png",
-			    		eth_url : "eth.png"
-				    });
-			    });
-		    });
-	    });
+    call_api_btc_1(function(doneAPIbtc){
+    	obj_btc_1 = doneAPIbtc;
+    });    	
+    call_api_eth_1(function(doneAPIeth){
+    	obj_eth_1 = doneAPIeth;
+    });    		
+	call_api_btc_2(function(doneAPIbtc2){
+		var obj_btc_bid_2_st = doneAPIbtc2['Realtime Currency Exchange Rate']['8. Bid Price'].toString();
+		obj_btc_bid_2 = parseFloat(obj_btc_bid_2_st).toFixed(2);
+		var obj_btc_ask_2_st = doneAPIbtc2['Realtime Currency Exchange Rate']['9. Ask Price'].toString();
+		obj_btc_ask_2 = parseFloat(obj_btc_ask_2_st).toFixed(2);
 	});
+	call_api_eth_2(function(doneAPIeth2){
+		var obj_eth_bid_2_st = doneAPIeth2['Realtime Currency Exchange Rate']['8. Bid Price'].toString()
+		obj_eth_bid_2 = parseFloat(obj_eth_bid_2_st).toFixed(2)
+		var obj_eth_ask_2_st = doneAPIeth2['Realtime Currency Exchange Rate']['9. Ask Price'].toString()
+		obj_eth_ask_2 = parseFloat(obj_eth_ask_2_st).toFixed(2)
+    });	
+	res.render('home.handlebars', {
+		btc_bid_1: obj_btc_1.bid,
+		btc_ask_1: obj_btc_1.ask,
+		eth_bid_1: obj_eth_1.bid,
+		eth_ask_1: obj_eth_1.ask,
+		btc_bid_2: obj_btc_bid_2,
+		btc_ask_2: obj_btc_ask_2,
+		eth_bid_2: obj_eth_bid_2,
+		eth_ask_2: obj_eth_ask_2,
+		btc_bid_min : parseFloat(obj_btc_1.bid)<=obj_btc_bid_2 ? "Bitstamp" : "Alpha Vantage",
+		btc_ask_max : parseFloat(obj_btc_1.ask)>=obj_btc_ask_2 ? "Bitstamp" : "Alpha Vantage",
+		eth_bid_min : parseFloat(obj_eth_1.bid)<=obj_eth_bid_2 ? "Bitstamp" : "Alpha Vantage",
+		eth_ask_max : parseFloat(obj_eth_1.ask)>=obj_eth_ask_2 ? "Bitstamp" : "Alpha Vantage",
+		btc_url : "btc.png",
+		eth_url : "eth.png"
+    });
 });
-
 
 hbs.registerHelper("apiNameAV", function(name) {
 	return name=="Alpha Vantage"
